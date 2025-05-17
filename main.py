6326ff91405147603bae6a5216592a1123c226af
wasabina67 <wasabina67@gmail.com>
2025-05-17 21:53:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("Example", debug=True)
+
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    return a + b
+
+
 def main():
-    print("Hello from mcp-server-python-example!")
+    print("Starting MCP server in stdio mode.")
+    mcp.run(transport="stdio")
 
 
 if __name__ == "__main__":
